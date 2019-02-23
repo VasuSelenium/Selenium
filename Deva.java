bb551de9ab3fddc15a59ef3d24480fc7c34bf368
VasuSelenium <47908160+VasuSelenium@users.noreply.github.com>
2019-02-22 21:48:59 -0500

    Add files via upload

---
+++ Deva.java
@@ -0,0 +1,16 @@
+package com.vasu.java;
+
+public class Deva extends Vasu
+{
+public void add(int i,int j)
+{
+	System.out.println(i-j);
+}
+	public static void main(String[] args)
+	{
+		
+Deva v=new Deva();
+v.add(70, 20);
+	}
+
+}
